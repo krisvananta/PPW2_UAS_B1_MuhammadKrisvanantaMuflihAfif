8f3ac3dd37a2f536bb107012a82d318777a9b6c0
krisvananta <afifkrisvananta@gmail.com>
2024-12-03 13:17:53 +0700

    bugfix nomor #7

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -4,22 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaksi extends Model
 {
-    use HasFactory
+    use HasFactory, SoftDeletes;
 
     protected $table = 'transaksi';
 
-    $fillable = [
+    protected $fillable = [
         'tanggal_pembelian',
         'total_harga',
         'bayar',
         'kembalian',
     ];
 
+    // protected $dates = ['deleted_at'];
+
     public function transaksidetail()
     {
-        return$this-hasMany(TransaksiDetail::class,'id_transaksi','id');
+        return $this->hasMany(TransaksiDetail::class,'id_transaksi','id');
     }
 }

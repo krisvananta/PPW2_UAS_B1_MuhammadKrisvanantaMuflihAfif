3539dea5d0c6c1ee7da5272628f330ad4c903bba
krisvananta <afifkrisvananta@gmail.com>
2024-12-03 13:43:48 +0700

    bugfix nomor #11

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
+use App\Models\TransaksiDetail;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $transaksi_count = Transaksi::count();
+
+        $jumlah_item_terjual = TransaksiDetail::sum('jumlah');
+
+        $omzet = TransaksiDetail::sum('subtotal');
+
+        return view('dashboard', compact('transaksi_count'));
     }
 }
